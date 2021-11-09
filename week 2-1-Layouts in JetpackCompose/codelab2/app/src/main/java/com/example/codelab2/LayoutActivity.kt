f6dfc7229942f3f5268480d1c13dcbd98da9eadc
parkhongsoon <kongsooni39@gmail.com>
2021-11-10 07:23:34 +0900

    Add CodeLab Layouts in JetpackCompose
    Add CodeLab Layouts in JetpackCompose

---
+++ week 2-1-Layouts in JetpackCompose/codelab2/app/src/main/java/com/example/codelab2/LayoutActivity.kt
@@ -0,0 +1,39 @@
+package com.example.codelab2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.example.codelab2.ui.theme.Codelab2Theme
+import kotlinx.coroutines.launch
+
+class LayoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Codelab2Theme {
+
+            }
+        }
+    }
+}
+
+
